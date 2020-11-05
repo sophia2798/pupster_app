7fe9c67ed4b69f544486f4c80be4b9b351a45cae
Jonny K <Kempj2.jk@gmail.com>
2020-11-05 11:26:02 -0800

    file structure in place

---
+++ src/components/discover/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import API from "../../utils/API"
+
+class discover extends Component {
+    state = {
+     
+    };
+    getDog =() =>{
+        API.search(query)
+      .then(res => this.setState({ results: res.data.data }))
+      .catch(err => console.log(err));
+    }
+  
+    render() {
+      return (
+        <h1>Stuff</h1> 
+      );
+    }
+  }
+  
+  export default discover;
+  
