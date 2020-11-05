cb1fe6c662fc843190e3d19ae170a9c987312016
Jonny K <Kempj2.jk@gmail.com>
2020-11-05 12:57:18 -0800

    functionality in place

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Discover from './components/pages/discover'
 
 function App() {
   return (
   <Router>
     <div className="App">
       <Navbar />
+      <Discover />
     </div>
   </Router>
 
--- src/components/pages/discover/index.js
+++ src/components/pages/discover/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import API from "../../../utils/API"
+import './style.css'
 
 class Discover extends React.Component {
     state = {
      dogImg:"",
      pupfriends:0,
-     click:""
     };
     
     componentDidMount() {
@@ -18,22 +18,30 @@ class Discover extends React.Component {
         .catch(err => console.log(err));
     }
     handleClick = (event) =>{
-      if(event.target.className === "like"){
-        // const oneInFive = Math.floor(Math.random() * Math.floor(5))
-        // return 
+      if(event.target.className === "btn btn-success like"){
+         const oneInFive = Math.floor(Math.random() * Math.floor(5))
+         if(oneInFive === 1){
+            this.setState(prevState => {
+            return {pupfriends: prevState.pupfriends + 1}
+         })
+         }
       }
-      this.setState({click:event.target.classNam })
+      this.getDog()
     }
   
     render() {
       return (
-        <div>
-          <h2>Make New Friends</h2>
-          <h3>Thumbs up on any pups you'd like to meet!</h3>
-          <img src={this.state.dogImg}/>
-          <h4 className="like" onClick={this.handleClick}>like</h4>
-          <h4 className="dislike" onClick={this.handleClick}>dislike</h4>
-          <h2>made friends with {this.state.pupfriends} pups so far</h2>
+        <div className="row">
+          <div className="col">
+            <h2>Make New Friends</h2>
+            <h3>Thumbs up on any pups you'd like to meet!</h3>
+            <img src={this.state.dogImg}/>
+            <div className="buttons">
+              <button type="button" onClick={this.handleClick} className="btn btn-success like">like</button>
+              <button type="button" onClick={this.handleClick} className="btn btn-danger dislike">pass</button>
+            </div>
+            <h2>made friends with {this.state.pupfriends} pups so far</h2>
+          </div>
         </div> 
       );
     }
